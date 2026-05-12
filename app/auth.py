fb9212d44b2f9ebc290f873a1f7383a2adbd217a
aruku192 <your@email.com>
2026-05-12 14:57:10 +0900

    feat: add user endpoints

--- app/auth.py
+++ app/auth.py
@@ -3,8 +3,12 @@
 
 
 
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
 
+
+def 
 
 
 def verify_token(token):
-    
+    pass
---
+++ app/roters.py
@@ -0,0 +1,37 @@
+
+
+from fastapi import APIRouter,Depends
+from database import SessionLocal
+from sqlalchemy.orm import Session
+import crud
+import schemas
+
+router = APIRouter()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@router.post("/users")
+async def create_user_endpoint(user:schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db,user)
+
+
+@router.get("/users/{user_id}")
+async def get_user_endpoint(user_id: int, db: Session = Depends(get_db)):
+    return crud.get_users(db,user_id)
+
+
+@router.patch("/users/{user_id}")
+async def update_user_endpoint(user_id: int, user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.update_user(db,user,user_id)
+
+@router.delete("/users/{user_id}")
+async def delete_user_endpoint(user_id: int, db: Session = Depends(get_db)):
+    crud.delete_user(db,user_id)
+    return {"message":"deleted"}
